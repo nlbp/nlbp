6a92a467386c6f9bc33fe50d9b950c58b3fbf2f6
parmote <parmote_tab@hotmail.com>
2019-03-06 09:55:25 +0700

    update master
    update master

--- resources/views/system/passport/client.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<passport-clients></passport-clients>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,30 +18,3 @@
 Auth::routes(['register' => false]);
 
 Route::get('/', 'HomeController@index')->name('home');
-
-// books management.
-Route::post('books/results', 'Books\BookController@results');
-Route::get('books/results', 'Books\BookController@results')->name('bookresults');
-Route::get('books/{id}/remove', 'Books\BookController@remove')->where('id', '[0-9]+')->name('removebook');
-Route::resource('books', 'Books\BookController');
-
-// search book
-Route::get('search', 'Books\SearchController@search')->name('SearchBook');
-Route::get('search/results', 'Books\SearchController@results')->name('SearchResults');
-Route::post('search/results', 'Books\SearchController@results');
-Route::get('search/{id}', 'Books\SearchController@show')->name('ShowSearch');
-
-// reading
-Route::get('reading', 'Reading\PersonController@index');
-Route::get('reading/person/create', 'Reading\PersonController@create');
-Route::post('reading/person/store', 'Reading\PersonController@store');
-Route::get('reading/person/checkbook', 'Reading\PersonController@checkBook');
-Route::get('reading/person/confirm', 'Reading\PersonController@confirm');
-Route::get('reading/person/{id}', 'Reading\PersonController@show')->where('id', '[0-9]+');
-Route::put('reading/person/{id}', 'Reading\PersonController@updateStatus')->where('id', '[0-9]+');
-Route::get('reading/settings/status', 'Reading\StatusController@index')->middleware('role:admin');
-Route::post('reading/settings/status', 'Reading\StatusController@store');
-
-// systems
-Route::get('setting/passport/client', 'System\SettingController@passportClients');
-Route::get('setting/passport/authorized', 'System\SettingController@passportAuthorized');
