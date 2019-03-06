0a56aee77863a125a152acb453f13e63ae647dcc
parmote <parmote_tab@hotmail.com>
2019-03-06 15:41:36 +0700

    update master
    update master

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,134 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/nexmo-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\NexmoChannelServiceProvider',
+    ),
+  ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
+  'laravel/slack-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\SlackChannelServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'rap2hpoutre/fast-excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rap2hpoutre\\FastExcel\\Providers\\FastExcelServiceProvider',
+    ),
+  ),
+  'yajra/laravel-acl' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\Acl\\AclServiceProvider',
+    ),
+  ),
+  'yajra/laravel-datatables-buttons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\ButtonsServiceProvider',
+    ),
+  ),
+  'yajra/laravel-datatables-editor' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\EditorServiceProvider',
+    ),
+  ),
+  'yajra/laravel-datatables-fractal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\FractalServiceProvider',
+    ),
+  ),
+  'yajra/laravel-datatables-html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\HtmlServiceProvider',
+    ),
+  ),
+  'yajra/laravel-datatables-oracle' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\DataTablesServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'DataTables' => 'Yajra\\DataTables\\Facades\\DataTables',
+    ),
+  ),
+);
---
+++ storage/framework/views/462888ed6b36db66706f45ff870e70fd41ef54bc.php
@@ -0,0 +1,54 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                <h1>Nlbp data</h1>
+                </div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+                    
+                    <h1><?php echo app('translator')->getFromJson('SearchMessages.BookLatest'); ?></h1>
+                <?php $__currentLoopData = $book; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php if($data->book->book_set == 1): ?>
+                <h3><a href="<?php echo e(url('search', ['id' => $data->book->aid])); ?>"><?php echo e($data->btitle); ?> <?php echo app('translator')->getFromJson('bookmessages.bookset'); ?></a></h3>
+                <?php else: ?>
+                <h3><a href="<?php echo e(url('search', ['id' => $data->book->aid])); ?>"><?php echo e($data->btitle); ?></a></h3>
+                <?php endif; ?>
+                <ul>
+                <?php if($data->bdatein ==! null): ?>
+                <li><?php echo app('translator')->getFromJson('bookmessages.datein', ['date' => $data->bdatein]); ?></li>
+                <?php endif; ?>
+                <?php if($data->bauthor ==! null): ?>
+                <li><?php echo app('translator')->getFromJson('bookmessages.author', ['name' => $data->bauthor]); ?></li>
+                <?php endif; ?>
+                <?php if($data->btranslate ==! null): ?>
+                <li><?php echo app('translator')->getFromJson('bookmessages.translate', ['name' => $data->btranslate]); ?></li>
+                <?php endif; ?>
+                <?php if($data->bpublish ==! null): ?>
+                <li><?php echo app('translator')->getFromJson('bookmessages.publish', ['name' => $data->bpublish]); ?></li>
+                <?php endif; ?>
+                <?php if($data->bread ==! null): ?>
+                <li><?php echo app('translator')->getFromJson('bookmessages.readby', ['name' => $data->bread]); ?></li>
+                <?php endif; ?>
+                </ul>
+                <?php echo e($data->bdetail); ?>
+
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
